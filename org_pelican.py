eafd2ff1b79dfea6ab974755d26d4e1be9adb1d0
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-05-14 20:38:12 +0800

    org_pelican.py: clean code
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- org_pelican.py
+++ org_pelican.py
@@ -1,6 +1,7 @@
-""" org-pelican extensions
+"""org-pelican extensions
 
-This pelican plugins add support to copy .org file to output folder.
+This pelican plugins add extra support for org-pelican such as copy .org sources
+to output folder.
 
 """
 from __future__ import unicode_literals, print_function
@@ -23,7 +24,7 @@ class OrgFileGenerator(Generator):
     def strip_suffix(self, filename):
         return os.path.splitext(filename)[0]
 
-    def copy_file(self, src, dst):
+    def publish(self, src, dst):
         dst_dirs = os.path.dirname(dst)
         if not os.path.exists(dst_dirs):
             os.makedirs(dst_dirs)
@@ -34,14 +35,12 @@ class OrgFileGenerator(Generator):
         if obj.source_path.endswith('.html') or obj.source_path.endswith('.md'):
             filename = self.strip_suffix(obj.source_path)
 
-            if os.path.isfile(filename + '.org'):
-                # print("-+++ --------> " + filename + '.org')
-                # print("slug ->>> " + obj.slug)
-                print("url ->>> " + self.strip_suffix(obj.url))
-                print("output_path " + self.output_path)
-                self.copy_file(filename + '.org', os.path.join( self.output_path, self.strip_suffix(obj.url) + '.org'))
+            org_file = filename + '.org'
+            org_dist =  self.strip_suffix(obj.url) + '.org'
 
             # Copy .org file with .html (which generate by org-pelican)
+            if os.path.isfile(org_file):
+                self.publish(org_file, os.path.join( self.output_path, org_dist))
 
     def generate_context(self):
         pass
@@ -49,7 +48,7 @@ class OrgFileGenerator(Generator):
     def generate_output(self, writer=None):
         # we don't use the writer passed as argument here
         # since we write our own files
-        logger.info('Copy xxx.org.html files....')
+        logger.info('Copy .org source files....')
 
         for article in self.context['articles']:
             self.copy_org_html(article)
